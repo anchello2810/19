3390cb14fa85c58d23e363e767dee011d813cd48
Anna Akopyan <anchello@yandex.ru>
2021-06-22 11:08:02 +0300

    Добавьте файлы проекта.

---
+++ 19.cpp
@@ -0,0 +1,42 @@
+﻿
+
+#include <iostream>
+class Animal
+{
+public:
+    virtual void Voice() {}
+
+};
+class Dog :public Animal
+{
+    void Voice() override
+    {
+        std::cout << "Woof\n";
+    }
+};
+class Cat :public Animal
+{
+    void Voice() override
+    {
+        std::cout << "Meow\n";
+    }
+};
+class Cow :public Animal
+{
+    void Voice() override
+    {
+        std::cout << "Mooo\n";
+    }
+};
+int main()
+{
+    Animal* animals[3];
+    animals[0] = new Dog();
+    animals[1] = new Cat();
+    animals[2] = new Cow();
+
+    for (Animal* a : animals)
+        a->Voice();
+}
+
+
